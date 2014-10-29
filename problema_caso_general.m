4610e1e7bd536d774dca5bf16faf529594b3153c
sscruz <s.scruz.312@gmail.com>
2014-10-29 22:29:49 +0100

    Archivo añadido

---
+++ problema_caso_general.m
@@ -0,0 +1,67 @@
+% Resolucion del problema -u'' = f(x) (en x \in (extremoIzd, extremodrch))
+% u(0) = alpha
+% u(1) = beta
+%
+% uExacta: solucion exacta al problema
+% N: numero de puntos interiores en la discretizacion 
+
+%%%%%%%%%%%%%%%%
+%%%% Datos: %%%%
+%%%%%%%%%%%%%%%%
+	extremoIzd = 0;
+	extremoDrch = 1;
+	
+	alpha = 0;
+	beta = 0;
+
+	uExacta = @(x) x.*sin(pi*x);
+	f = @(x) -2*pi*cos(pi*x) + pi*pi*x.*sin(pi*x);
+a = @(x) ;
+b = @(x) ;
+c = @(x) ;
+
+	N = 200;
+
+	h = (extremoDrch - extremoIzd)/(N+1);
+	x = extremoIzd:h:extremoDrch;
+	x = x';
+
+	%puntos interiores
+	xi = x(2:N+1);		
+
+
+%%%%%%%%%%%%%%%%%%%
+%%%% Programa: %%%%
+%%%%%%%%%%%%%%%%%%%
+
+	%Calculo de matrices y vectores
+        x1 = xi(2:N);
+        x2 = xi(1:N-1);
+	A = sparse(1:N-1,2:N,-a(x1)/(h*h)-b(x1)/(2*h),N,N)';
+        A += sparse(1:N-1,2:N,-(a(x2)/(h*h)-b(x2)/(2*h)),N,N);
+        A += sparse(1:N,1:N,2*a(xi)/(h*h)+c(x1),N,N);
+
+        R = f(xi);
+        R(1) = R(1) + a(xi(1))*alpha/(h*h) + b(xi(1))*alpha/(2*h);
+        R(N) = R(N) + a(xi(N))*beta/(h*h) - b(xi(N))*beta/(2*h);
+
+
+	% resolucion del problema
+	uAproximada = A\R;
+	
+	% calculo del error
+
+%%%%%%%%%%%%%%%%%%
+%%%% Dibujos: %%%%
+%%%%%%%%%%%%%%%%%%
+	% Limpiar dibujos previos
+	close 
+
+	%Dibujar
+	plot(x,[alpha;uAproximada;beta],'*');
+	axis([extremoIzd, extremoDrch, min(uAproximada)-0.2, max(uAproximada)+0.2])	
+	hold on
+
+	plot(x,uExacta(x),'r');
+
+	hold off;
