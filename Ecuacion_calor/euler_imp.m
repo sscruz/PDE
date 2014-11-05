63859aca1f693c8b3cfd929370e79257f7e4b852
sscruz <s.scruz.312@gmail.com>
2014-11-05 23:54:44 +0100

    Name of file changed

--- Ecuacion_calor/euler_imp.m
+++ Ecuacion_calor/euler_imp.m
@@ -1,12 +1,14 @@
-%% Resolucion numerica de la eq del calor
-%% u_t (x,t) -nu*u_xx(x,t) = f(x,t)
-%% u(
+%% Resolucion numerica de la eq del calor usando met. Euler implicito
+%% u_t (x,t) -nu*u_xx(x,t) = fuente(x,t) en a<x<b t<T
+%% u(a,t) = alpha(t)
+%% u(b,t) = beta(t)
+%% u(x,0) = uInicial(x)
 
 
 %%%%%%%%%%%
 %% Datos %%
 %%%%%%%%%%%
-    
+
 a = 0;
 b = 1;
 T =2;
@@ -16,7 +18,7 @@ uInicial = @(x) 10*(0.25-x).*(x-0.75).*(x>0.25).*(x<0.75);
 alpha = @(t) sin(4*pi*t);
 beta = @(t) 4*t.*(T-t);
 nu = 1;
-fuente = @(x,t) x.*t;
+fuente = @(x,t) x.*t;		
 
 
 
@@ -46,19 +48,19 @@ PlotU = [alpha(0);u0;beta(0)];
 plot(x,PlotU);
 
 for k=1:M,
-b = tau*fuente(xi,tau*k);
-b(1) = lambda*alpha(k*tau);
-b(end) = lambda*beta(k*tau);
-
-b += AllU(:,k);
-AllU(:,k+1) = A\b;
-PlotTmp = [alpha(k*tau);AllU(:,k+1);beta(k*tau)];
-PlotU = [PlotU, PlotTmp];
-
-hold on
-plot(x,PlotU(:,k+1));
-pause(.1);
-
+  b = tau*fuente(xi,tau*k);
+  b(1) = lambda*alpha(k*tau);
+  b(end) = lambda*beta(k*tau);
+  
+  b += AllU(:,k);
+  AllU(:,k+1) = A\b;
+  PlotTmp = [alpha(k*tau);AllU(:,k+1);beta(k*tau)];
+  PlotU = [PlotU, PlotTmp];
+  
+  hold on
+  plot(x,PlotU(:,k+1));
+  pause(.1);
+  
 end
 
 
